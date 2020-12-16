b0a49fac5d9b6a7af9ae707e31c307b4e89db06e
Mikhail <ilchenkomik@yandex.ru>
2020-12-16 22:23:35 +0300

    1 commit

--- TestTask/ViewController.swift
+++ TestTask/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     @IBOutlet weak var tableView: UITableView!
     
-    
+    //check
     override func viewDidLoad() {
         super.viewDidLoad()
         
